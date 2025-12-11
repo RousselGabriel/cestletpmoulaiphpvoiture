8708dc9bcfee6967551c329815d4c417be307133
pmoulai <philippe.moulai@ac-grenoble.fr>
2025-12-11 21:28:12 +0100

    update insert

--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -10,14 +10,14 @@ public class Ihm {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Connector is installed");
             System.out.println("Try to connect to MySQL");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/toto", "phil", "phil");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/testdb", "bob", "bob");
             PreparedStatement pst = con.prepareStatement("select * from users");
             ResultSet rs = pst.executeQuery();
             System.out.println("Connection established");
             while (rs.next()) {
                 System.out.println(rs.getString("name"));
             }
-            pst = con.prepareStatement("INSERT INTO users (id, name, firstname) VALUES (NULL, 'DEVILLIERS--COUTOUT', 'Styvenn');");
+            pst = con.prepareStatement("INSERT INTO users (id, name, firstname) VALUES (NULL, 'Tonnot', 'bob');");
             result = pst.executeUpdate();
             if( result > 0) {
                 System.out.println("L'enregistrement a été ajouté");
